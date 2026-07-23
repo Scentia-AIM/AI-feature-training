f1247d1b0e1305141a18d8fc56d236c285f0815c
Payal Parikh <pparikh@scentia.com.au>
2026-07-23 16:12:02 +1000

    fixed the modal javascript

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
-// Brand Modals- AIM
+// Modal JS
 
-// Get the modal
-var modal = document.getElementById("AIMmodal");
+const boxes = document.querySelectorAll(".brandbutton");
+const closeButtons = document.querySelectorAll(".close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("AIMbrandbutton");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    const modalId = box.getAttribute("data-modal");
+    const modal = document.getElementById(modalId);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+  });
+});
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+closeButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    button.closest(".modal").style.display = "none";
+  });
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
-// Brand Modals- ABS
-
-// Get the modal
-var modal2 = document.getElementById("ABSmodal");
-
-// Get the button that opens the modal
-var btn2 = document.getElementById("ABSbrandbutton");
-
-// Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal2.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal2.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-}
 
 // Accordian JS for Feature Activation Headings
 document.querySelectorAll(".featureinstructionsheader").forEach((button) => {
